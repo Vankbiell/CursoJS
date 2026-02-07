d25665ae2bdfc880c0923d983eeebdadfbad3530
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2026-02-07 20:30:01 -0300

    Aula 11 - Condição Composta

---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+//Condição Composta
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if (pais = 'Brasil'){
+    console.log('Você é Estrangeiro')
+}else{
+    console.log('Você é Brasileiro')
+}
