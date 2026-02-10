8c45287b60bf4969f053c335683f08a041c6e6a8
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2026-02-10 12:37:55 -0300

    Aula 12 - Primeiro Exercicío Utilizando Condições

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,7 +1,19 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('foto')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var minutos = data.getMinutes()
+    //var hora = 19
+    msg.innerHTML = `Agora são ${hora}:${minutos} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'fotodia.jpg'
+        document.body.style.background = '#e2cd9f' 
+    }else if (hora >= 12 && hora < 18){
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#b9846f' 
+    }else{
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#515154' 
+    }
 }
