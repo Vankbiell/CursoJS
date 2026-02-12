1f72b471681108a35b55699039b46b6c7b614aef
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2026-02-12 12:49:40 -0300

    Aula 13 - Repetições de Teste no Final e Inicial

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -8,5 +8,33 @@ function verificar(){
     }else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src', 'bebeHomem.jpg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'homemJovem.jpg')
+            } else if (idade < 50){
+                img.setAttribute('src', 'adulto.jpg')
+            }else {
+                img.setAttribute('src', 'idoso.jpg')
+            }
+        }else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >=0 && idade < 10){
+                img.setAttribute('src', 'bebeMulher.jpg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'mulherJovem.jpg')
+            } else if (idade < 50){
+                img.setAttribute('src', 'mulherAdulta.jpg')
+            }else {
+                img.setAttribute('src', 'idosa.jpg')
+            }
+        }
+        res.innerHTML = `Identificamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
---
+++ Aula13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 3)
+
+/*
+var c = 1
+while(c <= 3){
+    console.log(`Passo ${c}`)
+    c++
+} */
