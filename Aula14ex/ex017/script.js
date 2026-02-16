70b2faa4c25bb641487085ec6a00cdf932d95847
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2026-02-16 12:10:30 -0300

    Exercício 2 da Aula 14 - Tabuada

---
+++ Aula14ex/ex017/script.js
@@ -0,0 +1,18 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0 ){
+        window.alert('Preencha o campo, ele está vazio')
+    }else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1;c <= 10;c++){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
+    }
+    
+
+}
