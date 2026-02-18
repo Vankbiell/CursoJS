b581bc2b2ff3e7b71ed195ba0372bccc80ac7ad7
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2026-02-18 15:11:56 -0300

    save

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -1,5 +1,11 @@
 function adicionar(){
     let num = document.getElementById('txtn')
     let n = Number(num.value)
-    window.alert(n)
+    if(n <=1 || n >= 100){
+        window.alert('Os números tem que ser entre 1 à 100')
+    }else{
+        let item = document.createElement('option')
+        item.text = `O seu número é ${n}`
+        
+    }
 }
